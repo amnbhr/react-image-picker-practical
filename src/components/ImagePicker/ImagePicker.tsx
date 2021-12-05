4d83ca7ae5ff0211e6bc0599987a756bbfa1f680
amnbhr <aminbhr.pe@gmail.com>
2021-12-05 11:45:17 +0330

    in the name of god

---
+++ src/components/ImagePicker/ImagePicker.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+interface ImagePickerProps {
+    title: string;
+}
+
+const ImagePicker = (props: ImagePickerProps) => {
+    const {title} = props;
+
+    return (
+        <div>
+            <span>
+                {title}
+            </span>
+
+            <input
+                type="file"
+                accept="image/png, image/jpeg"
+                onChange={(event) => console.log(event)}
+            />
+        </div>
+    )
+}
+
+export default ImagePicker;
+
+ImagePicker.defaultProps = {
+    title: 'Pick Image'
+}
